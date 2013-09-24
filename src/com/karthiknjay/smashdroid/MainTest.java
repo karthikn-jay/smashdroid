134324c363c250aa1e038269d94875833b10aba9
Karthik <karthik.jm85@gmail.com>
2013-09-24 16:33:55 +0530

    Update MainTest.java

--- src/com/karthiknjay/smashdroid/MainTest.java
+++ src/com/karthiknjay/smashdroid/MainTest.java
@@ -8,7 +8,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Main Test : Smashdroid");
-		System.out.println("Aiming to create a ebook app for android from smashwords");
+		System.out.println("Aiming to create a ebook app for android which reads from smashwords.com repo");
 	}
 
 }
