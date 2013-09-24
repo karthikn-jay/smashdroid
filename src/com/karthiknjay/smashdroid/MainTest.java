2a7d17162a9fbffb7aa133a49cd279aeed2e645f
karthikeyan.j <karthik.jm85@gmail.com>
2013-09-24 16:25:21 +0530

    First commit

---
+++ src/com/karthiknjay/smashdroid/MainTest.java
@@ -0,0 +1,14 @@
+package com.karthiknjay.smashdroid;
+
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main Test : Smashdroid");
+		System.out.println("Aiming to create a ebook app for android from smashwords");
+	}
+
+}
